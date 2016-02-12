5c00d790c6d0502814c80583a593a86e157cbd09
Jon Lucas <jon.s.lucas@gmail.com>
2016-02-12 10:40:40 -0500

    Webpack/babel set up complete.

--- server.js
+++ server.js
@@ -6,13 +6,11 @@
     app.use(express.static('./dist'));
 
     app.use('/', (req, res)=>{
-        "use strict";
         res.sendFile(process.cwd() + '/client/html/index.html');
     });
 
     const port = process.env.PORT || 3000;
     app.listen(port, ()=>{
-        "use strict";
         console.log('App listening on port: '+port);
     });
 })();
